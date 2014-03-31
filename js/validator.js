be4d63747cb4a57cffd59e06cba9c845182330fb
Cina Saffary <cinas@spiceworks.com>
2014-03-31 16:40:07 -0500

    Fixes #3: validating on blur so empty required fields are invalidated.

--- js/validator.js
+++ js/validator.js
@@ -37,7 +37,7 @@
 
     this.toggleSubmit()
 
-    this.$element.on('input.bs.validator', ':input', $.proxy(this.validateInput, this))
+    this.$element.on('input.bs.validator blur.bs.validator', ':input', $.proxy(this.validateInput, this))
 
     this.$element.find('[data-match]').each(function () {
       var $this  = $(this)
