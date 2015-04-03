fb534e98ce461a11d7d2fd235bd40e0ecfda50e5
Sergey Kovalchuk <admin@vitam.in.ua>
2015-04-04 00:23:49 +0300

    Custom feedback icons as recommended

--- js/validator.js
+++ js/validator.js
@@ -69,8 +69,10 @@
       match: 'Does not match',
       minlength: 'Not long enough'
     },
-    feedback_success: 'glyphicon-ok',
-    feedback_error: 'glyphicon-warning-sign'
+    feedback: {
+      success: 'glyphicon-ok',
+      error: 'glyphicon-warning-sign'
+    }
   }
 
   Validator.VALIDATORS = {
@@ -186,8 +188,8 @@
       $group.addClass('has-error')
 
       $feedback.length
-        && $feedback.removeClass(this.options.feedback_success)
-        && $feedback.addClass(this.options.feedback_error)
+        && $feedback.removeClass(this.options.feedback.success)
+        && $feedback.addClass(this.options.feedback.error)
         && $group.removeClass('has-success')
     })
   }
@@ -201,8 +203,8 @@
     $group.removeClass('has-error')
 
     $feedback.length
-      && $feedback.removeClass(this.options.feedback_error)
-      && $feedback.addClass(this.options.feedback_success)
+      && $feedback.removeClass(this.options.feedback.error)
+      && $feedback.addClass(this.options.feedback.success)
       && $group.addClass('has-success')
   }
 
@@ -237,12 +239,10 @@
   }
 
   Validator.prototype.defer = function ($el, callback) {
-    var _this = this;
-    if (!this.options.delay) return callback.apply(_this)
+    callback = $.proxy(callback, this);
+    if (!this.options.delay) return callback()
     window.clearTimeout($el.data('bs.validator.timeout'))
-    $el.data('bs.validator.timeout', window.setTimeout(function () {
-      callback.apply(_this)
-    }, this.options.delay))
+    $el.data('bs.validator.timeout', window.setTimeout(callback, this.options.delay))
   }
 
   Validator.prototype.destroy = function () {
