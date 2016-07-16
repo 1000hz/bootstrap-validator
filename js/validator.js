306d4cc979cd3ce4a731a43d71104926a57f4593
Cina Saffary <itscina@gmail.com>
2016-07-16 02:05:49 -0500

    v0.11.3 🔙🔛🔝 once more

--- js/validator.js
+++ js/validator.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap (plugin): validator.js v0.11.2
+ * Bootstrap (plugin): validator.js v0.11.3
  * ========================================================================
  * The MIT License (MIT)
  *
@@ -67,7 +67,7 @@
     this.toggleSubmit()
   }
 
-  Validator.VERSION = '0.11.2'
+  Validator.VERSION = '0.11.3'
 
   Validator.INPUT_SELECTOR = ':input:not([type="hidden"], [type="submit"], [type="reset"], button)'
 
