6ddb98c67b61e22e5511ab62dcaf799c5f2892a2
Cina Saffary <itscina@gmail.com>
2017-01-05 20:02:48 -0600

    Docs: 0.11.9

--- dist/validator.js
+++ dist/validator.js
@@ -1,6 +1,6 @@
 /*!
- * Validator v0.11.8 for Bootstrap 3, by @1000hz
- * Copyright 2016 Cina Saffary
+ * Validator v0.11.9 for Bootstrap 3, by @1000hz
+ * Copyright 2017 Cina Saffary
  * Licensed under http://opensource.org/licenses/MIT
  *
  * https://github.com/1000hz/bootstrap-validator
@@ -15,7 +15,7 @@
   function getValue($el) {
     return $el.is('[type="checkbox"]') ? $el.prop('checked')                                     :
            $el.is('[type="radio"]')    ? !!$('[name="' + $el.attr('name') + '"]:checked').length :
-           $el.is('select[multiple]')  ? +$el.val() ? $el.val() : null                           :
+           $el.is('select[multiple]')  ? ($el.val() || []).length                                :
                                          $el.val()
   }
 
@@ -35,7 +35,7 @@
 
     this.$element.find('[data-match]').each(function () {
       var $this  = $(this)
-      var target = $this.data('match')
+      var target = $this.attr('data-match')
 
       $(target).on('input.bs.validator', function (e) {
         getValue($this) && $this.trigger('input.bs.validator')
@@ -50,7 +50,7 @@
     this.$element.attr('novalidate', true) // disable automatic native validation
   }
 
-  Validator.VERSION = '0.11.8'
+  Validator.VERSION = '0.11.9'
 
   Validator.INPUT_SELECTOR = ':input:not([type="hidden"], [type="submit"], [type="reset"], button)'
 
@@ -80,11 +80,11 @@
       }
     },
     'match': function ($el) {
-      var target = $el.data('match')
+      var target = $el.attr('data-match')
       return $el.val() !== $(target).val() && Validator.DEFAULTS.errors.match
     },
     'minlength': function ($el) {
-      var minlength = $el.data('minlength')
+      var minlength = $el.attr('data-minlength')
       return $el.val().length < minlength && Validator.DEFAULTS.errors.minlength
     }
   }
@@ -157,22 +157,22 @@
     $el.data('bs.validator.deferred', deferred)
 
     function getValidatorSpecificError(key) {
-      return $el.data(key + '-error')
+      return $el.attr('data-' + key + '-error')
     }
 
     function getValidityStateError() {
       var validity = $el[0].validity
-      return validity.typeMismatch    ? $el.data('type-error')
-           : validity.patternMismatch ? $el.data('pattern-error')
-           : validity.stepMismatch    ? $el.data('step-error')
-           : validity.rangeOverflow   ? $el.data('max-error')
-           : validity.rangeUnderflow  ? $el.data('min-error')
-           : validity.valueMissing    ? $el.data('required-error')
+      return validity.typeMismatch    ? $el.attr('data-type-error')
+           : validity.patternMismatch ? $el.attr('data-pattern-error')
+           : validity.stepMismatch    ? $el.attr('data-step-error')
+           : validity.rangeOverflow   ? $el.attr('data-max-error')
+           : validity.rangeUnderflow  ? $el.attr('data-min-error')
+           : validity.valueMissing    ? $el.attr('data-required-error')
            :                            null
     }
 
     function getGenericError() {
-      return $el.data('error')
+      return $el.attr('data-error')
     }
 
     function getErrorMessage(key) {
@@ -184,18 +184,18 @@
     $.each(this.validators, $.proxy(function (key, validator) {
       var error = null
       if ((getValue($el) || $el.attr('required')) &&
-          ($el.data(key) || key == 'native') &&
+          ($el.attr('data-' + key) !== undefined || key == 'native') &&
           (error = validator.call(this, $el))) {
          error = getErrorMessage(key) || error
         !~errors.indexOf(error) && errors.push(error)
       }
     }, this))
 
-    if (!errors.length && getValue($el) && $el.data('remote')) {
+    if (!errors.length && getValue($el) && $el.attr('data-remote')) {
       this.defer($el, function () {
         var data = {}
         data[$el.attr('name')] = getValue($el)
-        $.get($el.data('remote'), data)
+        $.get($el.attr('data-remote'), data)
           .fail(function (jqXHR, textStatus, error) { errors.push(getErrorMessage('remote') || error) })
           .always(function () { deferred.resolve(errors)})
       })
