b74a28b2d840d45477efa35dc7ffb6d518cb6964
adilshow <adilson_cabral@hotmail.com>
2017-01-13 16:10:52 +0000

    fixing options persistence at new calls
    the problem was that options were not been saved, because at merge of options (default, current, new) the current options were not considered as were lost on the process, "$this.data()" didn't work at least for this purpose.
    
    the second problem was that the constructor of new validator was depend of "if(!data)" that was only true on the first call, what caused to call with new options were never saved at data.

--- js/validator.js
+++ js/validator.js
@@ -377,11 +377,14 @@
   function Plugin(option) {
     return this.each(function () {
       var $this   = $(this)
-      var options = $.extend({}, Validator.DEFAULTS, $this.data(), typeof option == 'object' && option)
+      var dataOptions = $this.data() && $this.data()["bs.validator"] && $this.data()["bs.validator"].options
+                          ? $this.data()["bs.validator"].options : {};
+
+      var options = $.extend({}, Validator.DEFAULTS, $this.data(), dataOptions, typeof option == 'object' && option)
       var data    = $this.data('bs.validator')
 
       if (!data && option == 'destroy') return
-      if (!data) $this.data('bs.validator', (data = new Validator(this, options)))
+      $this.data('bs.validator', (data = new Validator(this, options)))
       if (typeof option == 'string') data[option]()
     })
   }
